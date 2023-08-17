a4b4ff5d927ac559f3eaff15a5239dcdf83a4222
sibekov <>
2023-08-17 00:05:38 +0000

    Create the Board Class with Initializer function, print,guess and add methods

--- run.py
+++ run.py
@@ -1,3 +1,41 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+scores={"computer":0,"player1":0}
+
+class Board:
+  """
+  The main board class. Sets the board sizze, the number of ships, the player1's name and the board type ie player1's board or computer's board.
+  Has methods for adding ships and guesses and printing the board.
+  """
+
+  def __init__(self,size,num_ships,name,type):
+    self.size=size
+    self.board=[["." for x in range(size)] for y in range(size)]
+    self.num_ships=num_ships
+    self.name=name
+    self.type=type
+    self.guesses=[]
+    self.ships=[]
+
+  def print(self):
+      for row in self.board:
+        print(" ".join(row))
+
+  def guess(self,x,y):
+      self.guesses.append((x,y))
+      self.board[x][y]="X"
+      if (x,y) in self.ships:
+        self.board[x][y]="*"
+        return "Hit"
+      else:
+        return "Miss"
+
+  def add_ship(self,x,y,type="computer"):
+      if len(self.ships)>=self.num_ships:
+        print("Error: you cannnot add any more ships!")
+
+      else:
+        self.ships.append((x,y))
+        if self.type=="player1":
+          self.board[x][y]="@"
+
